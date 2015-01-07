2af40d1d48b2f68842c9a9869acb6ce4d21c8a8f
Manu <bionikspoon@gmail.com>
2015-01-06 18:44:28 -0600

    added rss reader

--- framework/laravel/app/routes.php
+++ framework/laravel/app/routes.php
@@ -79,3 +79,26 @@ Route::get('csv', function()
 	}
 	return Scifi::all();
 });
+Route::get('rss', function()
+{
+	$source = 'http://rss.cnn.com/rss/cnn_topstories.rss';
+	$headers = get_headers($source);
+	$response = substr($headers[0], 9, 3);
+	if ($response == '404') {
+		return 'Invalid Source';
+	}
+	$data = simplexml_load_string(file_get_contents($source));
+
+	if (count($data) == 0)
+	{
+		return 'No posts';
+	}
+	$posts = '';
+	foreach ($data->channel->item as $item) {
+		$posts .= "<h1><a href='$item->link'>$item->title</a></h1>";
+		$posts .= "<h4>$item->pubDate</h4>";
+		$posts .= "<p>$item->description</p>";
+		$posts .= "<hr>";
+	}
+	return $posts;
+});

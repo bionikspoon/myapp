a76f3639c7bc2faea8c4846cd9595ee6e035c9be
Manu <bionikspoon@gmail.com>
2015-01-14 02:53:55 -0600

    working on some workflow improvements

--- framework/laravel/app/views/items/index.blade.php
+++ framework/laravel/app/views/items/index.blade.php
@@ -4,16 +4,50 @@ List Items
 @stop
 
 @section('content')
-<ul class="list-group">
+<div class="panel-group" id="index" role="tabslist" aria-multiselectable="false">
+	<div class="panel panel-default">
 	@foreach($items as $item)
-		<li class="list-group-item">
-			<h4 class="list-group-item-heading">{{$item->name}}</h4>
-			<p class="list-group-item-text">{{$item->description}}</p>
-			<p class="list-group-item-text">
-				<a class="btn btn-default btn-sm btn-primary" href="{{URL::route('items.show', $item->id)}}" role="button">Show</a>
-				<a class="btn btn-default btn-sm btn-default" href="{{URL::route('items.edit', $item->id)}}" role="button">Edit</a>
-				<a class="btn btn-default btn-sm btn-danger" href="{{URL::route('items.destroy', $item->id)}}" role="button">Delete</a>
-		</li>
+		<div class="panel-heading" role="tab" id="collapseListGroupHeading{{$item->id}}">
+			<h4 class="panel-title">
+				<a href="#collapseListGroup{{$item->id}}" data-toggle="collapse" aria-expanded="false" aria-controls="collapseListGroup{{$item->id}}"  data-parent="#index">
+					{{$item->name}}
+
+				</a><span class="badge">${{$item->price}}</span>
+			</h4>
+		</div>
+		<div id="collapseListGroup{{$item->id}}" class="panel-collapse collapse" role="tabpanel" aria-labelleddby="collapseListGroupHeading{{$item->id}}" aria-expanded="false">
+			<ul class="list-group">
+				<li class="list-group-item">
+					{{$item->description}}
+				</li>
+				<li class="list-group-item">
+					<ul class="nav nav-pills">
+						<li role="presentation" ><a href="{{URL::route('items.show', $item->id)}}">Show</a></li>
+						<li role="presentation" ><a href="{{URL::route('items.edit', $item->id)}}">Edit</a></li>
+						<li role="presentation" ><a href="{{URL::route('items.destroy', $item->id)}}">Delete</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
 	@endforeach
-</ul>
+	</div>
+</div>
+
+
 @stop
+<!-- <a href="#collapse-{{$item->id}}" data-toggle="collapse" data-parent="#index" aria-expanded="false" aria-controls="collapse-{{$item->id}}" class="list-group-item">
+		<span class="badge">${{$item->price}}</span>
+		<h4 id="heading-{{$item->id}}" role="tab" class="list-group-item-heading">
+			{{$item->name}}
+		</h4>
+		<p id="collapse-{{$item->id}}" role="tabpanel" aria-labelledby="heading-{{$item->id}}" class="collapse list-group-item-text">
+			{{$item->description}}
+			<br>
+			<span class="btn-group" role="group" aria-label="command">
+				<button type="button" class="btn btn-default">link</button>
+				<button type="button" class="btn btn-default">link</button>
+				<button type="button" class="btn btn-default">link</button>
+			</span>
+		</p>
+
+	</a> -->
